d778843a1dec9bc7aa38872a81a868d5c368025b
Vinícius <vinicius.matheus252@gmail.com>
2021-11-17 21:23:38 -0300

    Add loading state and simple error message

--- androidApp/src/main/java/io/vnicius/github/kmmtest/android/ui/home/HomeScreen.kt
+++ androidApp/src/main/java/io/vnicius/github/kmmtest/android/ui/home/HomeScreen.kt
@@ -1,21 +1,29 @@
 package io.vnicius.github.kmmtest.android.ui.home
 
+import android.util.Log
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.paging.LoadState
 import androidx.paging.PagingData
 import androidx.paging.compose.collectAsLazyPagingItems
 import coil.compose.rememberImagePainter
-import io.vnicius.github.kmmtest.android.ui.theme.Shapes
+import io.vnicius.github.kmmtest.android.R
 import io.vnicius.github.kmmtest.data.model.Pokemon
 import kotlinx.coroutines.flow.Flow
 
@@ -49,26 +57,75 @@ fun PokemonsList(pokemons: Flow<PagingData<Pokemon>>, modifier: Modifier = Modif
                 PokemonItem(it)
             }
         }
+
+        pokemonsItems.apply {
+            when {
+                loadState.append is LoadState.Loading ||
+                        loadState.refresh is LoadState.Loading -> {
+                    item { LoadingItem() }
+                }
+                loadState.refresh is LoadState.Error -> {
+                    (loadState.refresh as? LoadState.Error)?.error?.localizedMessage?.let {
+                        item { ErrorItem(message = it) }
+                    }
+                }
+            }
+        }
     }
 }
 
 @Composable
+fun LoadingItem(modifier: Modifier = Modifier) {
+    CircularProgressIndicator(
+        modifier = modifier
+            .fillMaxWidth()
+            .size(32.dp)
+            .padding(16.dp)
+            .wrapContentWidth(Alignment.CenterHorizontally)
+    )
+}
+
+@Composable
+fun ErrorItem(message: String, modifier: Modifier = Modifier) {
+    Text(
+        text = message,
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        color = Color.Red,
+        textAlign = TextAlign.Center,
+        fontStyle = FontStyle.Italic
+    )
+}
+
+@Composable
 fun PokemonItem(pokemon: Pokemon, modifier: Modifier = Modifier) {
-    Surface(modifier = Modifier.padding(horizontal = 20.dp, vertical = 8.dp)) {
-        Row {
+    Card(
+        modifier = Modifier
+            .padding(horizontal = 10.dp, vertical = 4.dp)
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(8.dp)
+    ) {
+        Row(modifier = Modifier
+            .clickable {
+                Log.d("pokemon", "pokemon: $pokemon")
+            }
+            .padding(horizontal = 10.dp, vertical = 4.dp)
+            .fillMaxWidth()) {
             Image(
-                painter = rememberImagePainter(data = pokemon.spriteUrl),
+                painter = rememberImagePainter(data = pokemon.spriteUrl) {
+                    placeholder(R.drawable.ic_pokeball)
+                    error(R.drawable.ic_pokeball)
+                    crossfade(true)
+                },
                 contentDescription = pokemon.name,
                 modifier = Modifier.size(50.dp)
             )
-            Spacer(
-                modifier = Modifier
-                    .width(12.dp)
-                    .fillMaxHeight()
-            )
             Text(
-                text = pokemon.name.capitalize(),
-                modifier = Modifier.align(Alignment.CenterVertically)
+                text = pokemon.name.replaceFirstChar { it.titlecase() },
+                modifier = Modifier
+                    .align(Alignment.CenterVertically)
+                    .padding(horizontal = 12.dp)
             )
         }
     }
